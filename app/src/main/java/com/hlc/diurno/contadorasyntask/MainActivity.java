b4453cd0888be8f2a105787e4abf4a2927a4101c
sergiosanpol <sergiosanpol@gmail.com>
2017-11-20 01:01:20 +0100

    Terminado contador

--- app/src/main/java/com/hlc/diurno/contadorasyntask/MainActivity.java
+++ app/src/main/java/com/hlc/diurno/contadorasyntask/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hlc.diurno.contadorasyntask;
 
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView cont;
     private Button start, stop;
     private boolean activo = false;
+    private ProgressBar bar;
+    private Contador contador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,33 +26,42 @@ public class MainActivity extends AppCompatActivity {
         cont = (TextView) findViewById(R.id.textView_cont);
         start = (Button) findViewById(R.id.button_comenzar);
         stop = (Button) findViewById(R.id.button_parar);
+        bar = (ProgressBar) findViewById(R.id.progressBar);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            bar.setMin(0);
+        }
+        bar.setMax(10);
     }
 
     public void comenzarCont(View v) {
-        Contador contador = new Contador();
-        contador.execute();
+        if(!activo) { //Si hay algún contador activo no inicia el hilo
+            contador = new Contador();
+            contador.execute();
+        }
+    }
+
+    public void cancelarCont(View v){
+        if(contador != null) {
+            contador.cancel(true);
+        }
     }
 
     // Async Task
 
     class Contador extends AsyncTask<Void, Integer, Void> {
 
-        private ProgressBar bar;
-
         @Override
         protected void onPreExecute() {
-            if (activo == false) {
-                activo = true;
-            } else {
-                this.onCancelled();
-            }
+            activo = true;
+            cont.setText("0");
+            bar.setProgress(0);
         }
 
         @Override
         protected Void doInBackground(Void... n) {
 
             for (int i = 0; i <= 10 && !isCancelled(); i++) {
-                cont.setText(i);
+                onProgressUpdate(i);
                 SystemClock.sleep(1000);
                 //publishProgress(i*100 / i);
             }
@@ -57,12 +69,26 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onProgressUpdate(Integer... porc) {
+        protected void onProgressUpdate(final Integer... porc) {
+            bar.setProgress(porc[0]);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    cont.setText(porc[0].toString());
+                }
+            });
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            activo = false;
         }
 
         @Override
         protected void onCancelled() {
-            cont.append("0");
+            if(activo == true) {
+                activo = false;
+            }
         }
 
     }
